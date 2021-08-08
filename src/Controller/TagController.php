c5d6c6915692f81d270f73b8a653ee0dca920bda
Carlos Artur <cang.carlos@gmail.com>
2021-08-08 10:20:42 -0300

    feat: bootstrap, namespace twig, tag search working

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use App\Entity\Tag;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TagController extends AbstractController
 {
-    #[Route('/tag/{name}', name: 'tag_list', requirements: ["name" => ".+"])]
-    public function index(?string $name = null): Response
+    #[Route('/tag', name: 'tag_list', requirements: ["name" => ".+"])]
+    public function index(Request $request): Response
     {
         $manager = $this->getDoctrine();
+        $name = $request->get('name');
 
         /** @var TagRepository */
         $repository = $manager->getRepository(Tag::class);
@@ -21,6 +23,6 @@ class TagController extends AbstractController
             ? $repository->findByName($name)
             : $repository->findAll();
 
-        return $this->render('tag/index.html.twig', compact('allTags'));
+        return $this->render('@tag/index.html.twig', compact('allTags'));
     }
 }
